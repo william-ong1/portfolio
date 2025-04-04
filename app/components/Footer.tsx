33beaa9e9f3832fe42f6c7622d3ece24835d548f
William Ong <wlo12489@gmail.com>
2025-04-03 20:24:26 -0700

    Updated projects and text colors

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = (): JSX.Element => {
   return (
-    <footer className="text-xs text-center py-10 lg:pb-52">
+    <footer className="text-xs text-center text-white py-10 lg:pb-52">
       © 2025 William Ong. All rights reserved.
     </footer>
   );
